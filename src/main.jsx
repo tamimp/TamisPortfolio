aa908298f8c68c1b8763b252a953b56eaf3f03ae
tamimp <tamipoliwoda@hotmail.com>
2024-10-06 18:54:45 -0400

    updated case on App

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter as Router } from "react-router-dom"; // Import BrowserRouter
-import App from "./App"; // Import your App component
+import App from "./App.jsx"; // Import your App component
 import "./index.css";
 
 createRoot(document.getElementById("root")).render(
